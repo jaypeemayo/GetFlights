e6f893fc2501206f1cae144e04eca3ebf188a3e9
jaype <jaypeemayo@gmail.com>
2017-02-16 15:32:30 +0800

    make localization work

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { AppModule } from './app/app.module';
+import {getTranslationProviders} from "./app/i18n-providers";
+
+getTranslationProviders().then(providers => {
+  const options = { providers };
+  platformBrowserDynamic().bootstrapModule(AppModule, options);
+});
